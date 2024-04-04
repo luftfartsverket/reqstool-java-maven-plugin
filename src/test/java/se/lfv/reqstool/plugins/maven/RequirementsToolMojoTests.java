3c115baec534be082df459d08485f332b77a63d2
Marcus Billgren <142776362+lfvbarcus@users.noreply.github.com>
2024-04-04 09:32:50 +0200

    feat: Adds test to create zip using reflection (#17)
    * feat: Adds test to create zip using reflection
    
    * feat: Update test data for zip to dummy files

--- src/test/java/se/lfv/reqstool/plugins/maven/RequirementsToolMojoTests.java
+++ src/test/java/se/lfv/reqstool/plugins/maven/RequirementsToolMojoTests.java
@@ -2,9 +2,17 @@
 package se.lfv.reqstool.plugins.maven;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+
 import org.junit.jupiter.api.Test;
 import com.fasterxml.jackson.databind.JsonNode;
 
@@ -36,6 +44,47 @@ class RequirementsToolMojoTests {
 		String combinedFileContentAsString = new String(combinedFileContent);
 
 		assertEquals(combinedFileContentAsString, combinedResult, "The combined annotations files should match");
+	}
+
+	@Test
+	void testCreateZip() throws Exception {
+
+		ClassLoader classLoader = getClass().getClassLoader();
+		URL resourcePath = classLoader.getResource("zip");
+		Path zipResourcePath = Paths.get(resourcePath.getFile());
+		File file = zipResourcePath.toFile();
+		String zipFileNameString = "reqstool-test-zip";
+
+		RequirementsToolMojo mojo = new RequirementsToolMojo();
+
+		Field outputDirectory = RequirementsToolMojo.class.getDeclaredField("outputDirectory");
+		Field datasetPath = RequirementsToolMojo.class.getDeclaredField("datasetPath");
+		Field zipFileName = RequirementsToolMojo.class.getDeclaredField("zipFilename");
+		Field failsafeReportsDir = RequirementsToolMojo.class.getDeclaredField("failsafeReportsDir");
+		Field surefireReportsDir = RequirementsToolMojo.class.getDeclaredField("surefireReportsDir");
+
+		outputDirectory.setAccessible(true);
+		datasetPath.setAccessible(true);
+		zipFileName.setAccessible(true);
+		failsafeReportsDir.setAccessible(true);
+		surefireReportsDir.setAccessible(true);
+
+		outputDirectory.set(mojo, file);
+		datasetPath.set(mojo, file);
+		zipFileName.set(mojo, zipFileNameString);
+		failsafeReportsDir.set(mojo, file);
+		surefireReportsDir.set(mojo, file);
+
+		Method zipMethod = RequirementsToolMojo.class.getDeclaredMethod("createZipFile");
+		zipMethod.setAccessible(true);
+
+		zipMethod.invoke(mojo);
+
+		Path zipfileTargetPath = Paths.get("target", "test-classes", "zip");
+		Path expectedZipFilePath = zipfileTargetPath.resolve(zipFileNameString);
+
+		assertTrue(Files.exists(expectedZipFilePath));
+
 	}
 
 }
